3982d309fa606f0f860d8e8752bb35e76c329285
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-02 16:11:53 -0700

    Added environment variables and proxy packages to handle corporate proxy.

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const https = require("https");
+const HttpProxyAgent = require('http-proxy-agent');
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
 app.get("/", function(req, res) {
-  res.send("Server is up and running");
+  let proxyServer = process.env.HTTP_PROXY;
+  let appid = process.env.API_KEY;
+  let apiEndpoint = "api.openweathermap.org"
+  let apiPath = "/data/2.5/weather?q=london&units=imperial&appid=" + appid;
+
+  let options = {
+    agent: new HttpProxyAgent(proxyServer),
+    host: apiEndpoint,
+    path: apiPath,
+    method: "GET",
+    headers: {
+      Host: "https://" + apiEndpoint
+    }
+  };
+
+  https.get(options, function(response) {
+    console.log(response);
+  });
+
+  res.send("Server is up and running.");
 });
-
-
 
 
 app.listen(port, function() {
