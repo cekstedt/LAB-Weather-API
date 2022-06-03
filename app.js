f608b2c6298a0a848329ce907e7d02c2ad5537dc
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-02 17:00:15 -0700

    Added form submission so that user can POST custom city and retrieve weather data for that city.

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const HttpsProxyAgent = require("https-proxy-agent");
 require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
   let proxyServer = process.env.HTTPS_PROXY;
   let appid = process.env.API_KEY;
   let apiEndpoint = "api.openweathermap.org";
-  let apiPath = "/data/2.5/weather?q=London,uk&units=imperial&appid=" + appid;
+  let query = req.body.cityName;
+  let apiPath = "/data/2.5/weather?q=" + query + "&units=imperial&appid=" + appid;
 
   let options = {
     agent: new HttpsProxyAgent(proxyServer),
@@ -30,14 +38,13 @@ app.get("/", function(req, res) {
       let imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.write('<head><meta charset="utf-8"></head><body>');
       res.write("<p>The weather is currently " + desc + ".</p>");
-      res.write("<h1>The temperature in London is " + temp + "° Farenheit.</h1>");
+      res.write("<h1>The temperature in " + query + " is " + temp + "° Farenheit.</h1>");
       res.write('<img src="' + imgURL + '" alt="' + desc + '">')
       res.write("</body>");
       res.send();
     });
   });
 });
-
 
 app.listen(port, function() {
   console.log("Server started on port " + port);
